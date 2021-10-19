d27e1f3afe4a6f5d54bd947d0b11c78e489f2cf3
Alex Lobera <alex.lobera@hopin.to>
2021-10-19 16:12:02 +0200

    add benchmark

---
+++ host/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const port = 8887;
+
+module.exports = {
+  mode: "development",
+  output: {
+    publicPath: `http://localhost:${port}/`,
+  },
+  devServer: {
+    port,
+    historyApiFallback: {
+      index: "/index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "host",
+      remotes: {
+        remote: "remote@http://localhost:8888/remoteEntry.js",
+      },
+      shared: { react: "^17.0.2" },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
--- microfrontends/host/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { UserProvider } from "user-provider";
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <UserProvider username="alexlbr">
-      <Component {...pageProps} />
-    </UserProvider>
-  );
-}
